d625794c0810f1142b494c572858b7b35bd89626
Jozark <jorekplus@gmail.com>
2022-10-03 13:17:46 +0200

    looping vectors

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
 fn main() {
-    // variables are on default immutable
-    let mut x: i8 = 10; 
-    println!("x is {}", x);
-    x = 5;
-    println!("x is {}", x);
-    let mut tf: bool = true;
-    let mut text = "first text";
-    println!("text is {} and boolean is {}", text, tf);
-    tf = false;
-    text = "second text";
-    println!("text is {} and boolean is {}", text, tf);
 
-    // tuples 
-    let mut t = ("hello",2,3);
-    println!("The values t are {}, {}, {}", t.0,t.1,t.2);
-    t.1 = 40;
-    println!("The values t are {}, {}, {}", t.0,t.1,t.2);
-    // deconstruction
-    let (a,_b,_c) = t;
-    println!("The value of a is {}", a);
+    // let mut t = ("hello",true, 3);
 
-    // arrays
-    let l = [1,2,3];
-    println!("The values l are {}, {}, {}", l[0],l[1],l[2]);
+    // let mut l = [1,2,3];
+    // for item in l {
+    //     println!("> {}", item)
+    // }
 
-    //vector
-    let mut vec = vec![1,2,4];
-    println!("Starting vector data is {:?}", vec);
-    vec.push(5);
-    println!("Starting vector data is {:?}", vec);
-    vec.pop();
-    println!("Starting vector data is {:?}", vec);
+    let vec = vec![1,2,4];
+    for data in vec.iter() {
+        if *data == 2 {
+            println!("/ {}", data)
+        }
+    }
 
 }
